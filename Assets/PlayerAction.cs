b24fe564a5073e732ecb208565b98614ee0a9270
aminyl <ellallopll@gmail.com>
2017-11-02 02:10:03 +0900

    no message

---
+++ Assets/PlayerAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour {
+	[SerializeField]
+	Vector3 v;
+	[SerializeField]
+	float power;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			rb.AddForce (v * power, ForceMode.Impulse);
+		}
+	}
+}
